be35ef29a72a03869318d3dc6b21c8738bff4865
Mitchell Monahan <soccermitchy@gmail.com>
2015-05-02 21:35:08 -0400

    Some of the rewrite of the original wiki

--- app.lua
+++ app.lua
@@ -1,8 +1,34 @@
-local lapis = require("lapis")
-local app = lapis.Application()
+-- Lapis stuff. Not global so I don't have to re-include them in different files.
+lapis       = require'lapis'              -- Web framework functions
+validate    = require'lapis.validate'     -- Input validation
+app_helpers = require'lapis.application'  -- Application stuff
 
-app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
-end)
+-- Function 'aliases'
+capture_errors = app_helpers.capture_errors
 
-return app
+-- The aplication
+app = lapis.Application()
+app:enable("etlua") -- templates
+app.layout=require'views.layout' 	-- The layout of the whole wiki.
+									-- views/layout.etlua
+
+function validate.validate_functions.is_email(email,value) -- Validate function: Check if input is an email
+	if value==true then
+		local err='%s must be an email.'
+	else
+		local err='%s must not be an email.'
+	end
+	return (email:match("[A-Za-z0-9%.%%%+%-]+@[A-Za-z0-9%.%%%+%-]+%.%w%w%w?%w?")~=nil)==value,err -- email:match(...) - Pattern for an email, ~=nil: true if there is an email, false if not. ==value: if the user wants it to be an email or not
+end
+
+function create_static_page(title,template) -- Creates a route function for a static page with an etlua template.
+	return function(self)
+		self.page_title=title or error('No title given (template: '..template..')')
+		return { render = template}
+	end
+end
+
+---------- Static Routes
+app:get("index","/",create_static_page("index","index"))
+app:get("register","/register",create_static_page("register","register"))
+app:get("login","/login",create_static_page("login","login"))
